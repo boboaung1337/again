3b177e3844d44d8db216865e6802934b73a7d126
boboaung1337 <160343524+boboaung1337@users.noreply.github.com>
2025-12-10 05:02:17 +0800

    Update gettgtpkinit.py

--- gettgtpkinit.py
+++ gettgtpkinit.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+#!/usr/bin/env -S uv run --script
 # /// script
 # requires-python = ">=3.13"
 # dependencies = [
@@ -355,3 +355,4 @@ def main():
 
 if __name__ == '__main__':
     main()
+
