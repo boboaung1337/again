1b3dd0fb7a6d98876b8befb97135f231e6d7bb4d
b0b0aung <160343524+boboaung1337@users.noreply.github.com>
2026-08-10 15:07:04 +0800

    Update decrypt.py

--- decrypt.py
+++ decrypt.py
@@ -1,60 +1,12 @@
-# /// script
-# requires-python = ">=3.13"
-# dependencies = [
-#     "pycryptodome>=3.23.0",
-# ]
-# ///
 from Crypto.Cipher import DES
 from Crypto.Util.Padding import unpad
 import base64
 
-''' Found in SmarterMail.Standard.dll (using dnSpy)
-keymap1 = { 125, 113, 232, 233, 160, 34, 123, 208 }, { 224, 222, 8, 14, 29, 138, 139, 223 });
-keymap2 = { 180, 63, 132, 209, 16, 180, 233, 145 }, { 1, 216, 174, 230, 73, 173, 146, 39 });
-'''
+KEY = bytes([180, 63, 132, 209, 16, 180, 233, 145])
+IV = bytes([1, 216, 174, 230, 73, 173, 146, 39])
 
-KEY = bytes([
-    180, 63, 132, 209,
-    16, 180, 233, 145
-])
-
-IV = bytes([
-    1, 216, 174, 230,
-    73, 173, 146, 39
-])
-
-
-def decrypt_password(password_encrypted):
-
-    print("[*] Key:", KEY.hex())
-    print("[*] IV:", IV.hex())
-
-    ciphertext = base64.b64decode(password_encrypted)
-
-    if len(ciphertext) % 8 != 0:
-        raise ValueError(
-            f"Invalid DES ciphertext length: {len(ciphertext)}"
-        )
-
-    cipher = DES.new(
-        KEY,
-        DES.MODE_CBC,
-        IV
-    )
-
-    plaintext = unpad(
-        cipher.decrypt(ciphertext),
-        DES.block_size
-    )
-
-    return plaintext.decode("utf-8")
-
-
-def main():
-    password_encrypted = "66e7ppLOBF7UdzDv7zK6MJ1rmyUb1Cby"
-    plaintext = decrypt_password(password_encrypted)
-    print(f"[+] Plaintext: {plaintext}")
-
-
-if __name__ == "__main__":
-    main()
+password_encrypted = "66e7ppLOBF7UdzDv7zK6MJ1rmyUb1Cby"
+ciphertext = base64.b64decode(password_encrypted)
+cipher = DES.new(KEY, DES.MODE_CBC, IV)
+plaintext = unpad(cipher.decrypt(ciphertext), DES.block_size)
+print(f"Password: {plaintext.decode('utf-8')}")
